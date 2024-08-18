9039142386c06bbfb973d5e42dd0f291488a4acd
Anirudh Kilambi <anirudh.kilambi@compassdigital.io>
2024-08-18 01:39:47 +0000

    update install.sh with required libraries for steam install

--- packages/install.sh
+++ packages/install.sh
@@ -15,3 +15,9 @@ pacman -S avrdude
 pacman -S arm-none-eabi-gcc
 pacman -S openconnect
 pacman -S dbeaver
+
+# steam stuff
+#pacman -S lib32-systemd
+pacman -S ttf-liberation # for arial font support in steam (as per Steam wiki entry)
+pacman -S lib32-nvidia-utils # 32 Bit version of OpenGL drivers for NVIDIA 
+pacman -S steam # no fucking shit sherlock
