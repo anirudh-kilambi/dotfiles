9b22c6a622a74208b83af99f4e6a946fb4dd9017
Anirudh Kilambi <anirudh.kilambi@compassdigital.io>
2025-11-18 20:38:29 -0500

    update nvim config with current arch prod config

--- nvim/config/lua/plugins/plugins.lua
+++ nvim/config/lua/plugins/plugins.lua
@@ -5,6 +5,8 @@ local plugin_files = {
   "copilot",
   "copilot-chat",
   "gitsigns",
+  "lspconfig",
+  "autocompletion",
 }
 
 local plugins = {}
@@ -24,28 +26,6 @@ return plugins
 
 
 
-
-  ---- Git signs
-  --{
-    --"lewis6991/gitsigns.nvim",
-    --opts = {
-      --signs = {
-        --add          = { text = "│" },
-        --change       = { text = "│" },
-        --delete       = { text = "_" },
-        --topdelete    = { text = "‾" },
-        --changedelete = { text = "~" },
-      --},
-    --},
-    --config = function(_, opts)
-      --require("gitsigns").setup(opts)
-      --vim.keymap.set("n", "]c", require("gitsigns").next_hunk, { desc = "Next hunk" })
-      --vim.keymap.set("n", "[c", require("gitsigns").prev_hunk, { desc = "Prev hunk" })
-      --vim.keymap.set("n", "<leader>hs", require("gitsigns").stage_hunk, { desc = "Stage hunk" })
-      --vim.keymap.set("n", "<leader>hu", require("gitsigns").undo_stage_hunk, { desc = "Undo stage" })
-      --vim.keymap.set("n", "<leader>hp", require("gitsigns").preview_hunk, { desc = "Preview hunk" })
-    --end,
-  --},
 
   ---- File tree
   --{
--- nvim/config/lua/settings/mappings.lua
+++ nvim/config/lua/settings/mappings.lua
@@ -1,11 +1,9 @@
 local map = vim.keymap.set --alias for mapping
 
 -- general mappings
-map("n", "<leader>cl", "<cmd>nohlsearch<CR>", { desc = "Clear search highlight" })
+map("n", "<leader>c", "<cmd>nohlsearch<CR>", { desc = "Clear search highlight" })
 map("n", "<leader>l", "<cmd>ls<CR>", {desc = "List buffers" })
 map("n", "<leader>w", ":ls<CR>:b <Space>", { desc = "Switch buffer" })
 map("n", "<leader>jj", ":%!jq .<CR>", { silent = true, desc = "Prettify JSON" })
-map("n", "<leader>co", ":CopilotChat<CR>", {silent = true, desc = "Open Copilot Chat" })
-map("n", "<leader>cc", ":CopilotChatClose<CR>", {silent = true, desc = "Open Copilot Chat" })
 
 
