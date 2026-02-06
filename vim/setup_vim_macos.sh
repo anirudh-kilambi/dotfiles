d212fdca70ac2dd6ce1dd114945a753b90e3d98f
Anirudh Kilambi <anirudh.kilambi@compassdigital.io>
2026-02-05 20:18:29 -0500

    add mac os specific things

---
+++ vim/setup_vim_macos.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+
+echo "----- Uninstalling default VIM and compiling from source-----"
+mkdir -p $HOME/.builds/
+git clone https://github.com/vim/vim.git
+cd $HOME/.builds/
+
+echo "----- Creating sym link to vimrc in root directory -----"
+base_dir=$(pwd)
+vimrc_directory="$HOME/code/repos/dotfiles/vim/vimrc"
+vim_default_settings_path="$HOME/code/repos/dotfiles/vim/default_settings.vim"
+echo $vimrc_directory
+
+ln -sf $vimrc_directory ~/.vimrc
+
+ls -la ~ | grep .vimrc
+
+vimrc_contents=$(cat ~/.vimrc | grep "$vim_default_settings_path")
+
+if [[ "$vimrc_contents" == "" ]]; then
+    echo "source $vim_default_settings_path" >> ~/.vimrc
+fi
+
+echo "----- installing plug.vim -----"
+plug_exists=$(ls ~/.vim/autoload/ | grep plug.vim)
+if [[ "$plug_exists" == "" ]]; then
+    echo "plug.vim not installed. Loading..."
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+else
+    echo "plug.vim exists in ~/.vim/autoload/. Not downloading."
+fi
+
+echo "----- installing nodejs -----"
+node_exists=$(brew install nodejs)
+if [[ "$node_exists" == "" ]]; then
+    echo "NodeJS not installed. Installing"
+    brew intall nodejs
+else
+    echo "NodeJS installed. Skipping."
+    brew install nodejs
+fi
+
+echo "----- Installing npm -----"
+npm_exists=$(brew install npm)
+if [[ "$npm_exists" == "" ]]; then
+    echo "NPM not installed. Installing"
+    brew install npm
+else
+    echo "NPM installed. Skipping."
+fi
+
+echo "----- adding plugins.vim file -----"
+plugins_exists=$(cat ~/.vimrc | grep plugins.vim)
+if [[ "$plugins_exists" == "" ]]; then
+    echo "Sourcing Plugins"
+    echo "call plug#begin() 
+    source $base_dir/plugins/plugins.vim 
+call plug#end()" >> ~/.vimrc
+else
+    echo "Plugins Already Sourced. Continue."
+fi
+
+echo "----- Installing Plugins -----"
+vim -c 'PlugInstall' -c 'qa!'
+
+echo "----- Sourcing plugin .vim files -----"
+files=$(ls plugins/)
+
+for f in $files; do
+    echo "File => $f"
+    exists=$(cat ~/.vimrc | grep $f)
+    if [[ $exists == "" ]]; then
+        echo "source $base_dir/plugins/$f" >> ~/.vimrc
+    else
+        echo "$f has already been sourced. Continue."
+    fi
+done
+
+# update lightline.vim
+# I use the simpleblack colorscheme for lightline.vim (installed with onedark.vim) with a couple of changes. I save them under `plugins/statusbar.vim`.
+# Need to update the simpleblack.vim colorscheme that lives in ~/.vim/plugged/lightline.vim/autoload/lightline/colorscheme/simpleblack.vim
+colorscheme="$base_dir/plugins/statusbar.vim"
+echo "updating colorscheme"
+
+cat $colorscheme > ~/.vim/plugged/lightline.vim/autoload/lightline/colorscheme/simpleblack.vim
+
+# fzf tings
+echo "Installing FD"
+brew install fd
+
+echo "Installing bat"
+brew install bat
+
+
+#updating zshrc with fzf commands
+default_cmd_exists=$(cat ~/.zshrc | grep 'export FZF_DEFAULT_COMMAND="fd . $HOME"')
+ctrl_t_cmd_exists=$(cat ~/.zshrc | grep 'export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"')
+preview_cmd_exists=$(cat ~/.zshrc | grep 'export FZF_PREVIEW_COMMAND="bat --style=numbers,changes --wrap never --color always {} || cat {} || tree -C {}"')
+
+if [[ $default_cmd_exists == "" ]]; then
+    echo 'export FZF_DEFAULT_COMMAND="fd . $HOME"' >> ~/.zshrc
+fi
+if [[ $ctrl_t_cmd_exists == "" ]]; then
+    echo 'export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"' >> ~/.zshrc
+fi
+if [[ $preview_cmd_exists == "" ]]; then
+    echo 'export FZF_PREVIEW_COMMAND="bat --style=numbers,changes --wrap never --color always {} || cat {} || tree -C {}"' >> ~/.zshrc
+fi
+
+exit 0
+
---
+++ zsh/generate_symlinks_macos.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Creating sym link to zshrc in root directory"
+zshrc_directory="$(pwd)/zshrc"
+aliases_path="$(pwd)/aliases"
+echo $zshrc_directory
+
+ln -sf $zshrc_directory ~/.zshrc
+
+ls -la ~ | grep .zshrc
+
+aliases_exists=$(cat ~/.zshrc | grep "$aliases_path")
+
+if [[ "$aliases_exists" == "" ]]; then
+    echo "aliases not sourced"
+    echo "source $aliases_path" >> ~/.zshrc
+else
+    echo "aliases already sourced. Exiting"
+fi
+
+exit 0
+
